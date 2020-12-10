a581f42cf6492230400b10e0310ff634635a601b
hyfung <43932013+hyfung@users.noreply.github.com>
2020-12-10 16:27:30 +0800

    Update main.c

--- Consecutive_Digit/main.c
+++ Consecutive_Digit/main.c
@@ -2,10 +2,18 @@
 Problem:
 Write a function to check for 3 or more consecutive digits in an integer.
 No loop, no array, recursion only
+Assume there is no leading 0
 
 Solution:
 Use the sliding window of the last 3 digits of a number
 Return true if they are equal, otherwise perform recursion on N/10 where N is the number
+
+Sample Test Cases:
+12345 -> No
+122223 -> Yes
+12321 -> No
+1222333 -> Yes
+12223334 -> Yes
 """
 
 

558506150f2f8134a8f36d1de5015496a3e58bda
hyfung <43932013+hyfung@users.noreply.github.com>
2020-12-13 17:13:08 +0800

    Create Solution.py

---
+++ 0028. Implement strStr()/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if haystack == "" and needle == "":
+            return 0
+        try:
+            return haystack.index(needle)
+        except:
+            return -1
+        

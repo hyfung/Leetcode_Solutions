36d52395cad890f418da8aa1ea4002a03cbbfb38
hyfung <43932013+hyfung@users.noreply.github.com>
2020-12-16 17:06:16 +0800

    Create Solution.py

---
+++ 0094. Binary Tree Inorder Traversal/Solution.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        ls = []
+        def traversal(node):
+            nonlocal ls
+            if node is None:
+                return
+            traversal(node.left)
+            ls.append(node.val)
+            traversal(node.right)
+        traversal(root)
+        return ls
+        

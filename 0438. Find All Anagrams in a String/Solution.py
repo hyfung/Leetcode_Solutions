1ae363cc99c7c8818e555069261960e0c3b6d307
hyfung <43932013+hyfung@users.noreply.github.com>
2020-12-16 11:47:39 +0800

    Update Solution.py

--- 0438. Find All Anagrams in a String/Solution.py
+++ 0438. Find All Anagrams in a String/Solution.py
@@ -1,18 +1,35 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        S, P = len(s), len(p)
+        
+        # S-P+1 round sliding window
+        # Using hashmap
+        
         def make_dict(string):
             pattern = dict()
+            for i in 'abcdefghijklmnopqrsztuvwxyz':
+                pattern[i] = 0
             for char in string:
-                pattern[char] = 1 if char not in pattern else pattern[char] + 1
+                pattern[char] += 1
             return pattern
         
         res = []
         
         p_dict = make_dict(p)
         
-        for i in range(0, len(s)-len(p) + 1):
-            s_tmp = s[i:i+len(p)]
-            if make_dict(s_tmp) == p_dict:
+        # Prime the dictionary
+        s_dict = make_dict(s[:len(p)])
+        
+        assert len(p_dict) == len(s_dict)
+        
+        for i in range(0, len(s)-len(p)+1):
+            if s_dict == p_dict:
                 res.append(i)
+            
+            try:
+                s_dict[s[i]] -= 1
+                s_dict[s[i+P]] += 1
+            except:
+                pass
         
         return res

b43958d801fbd06e37217d98d998e31ed7ff1c07
hyfung <43932013+hyfung@users.noreply.github.com>
2020-12-20 18:14:26 +0800

    Create Solution.py

---
+++ 0119. Pascal's Triangle II/Solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        res = []
+        """
+        Answer copied from Pascal's Triangle I with slight modifications
+        """
+        numRows = rowIndex + 1
+
+        for i in range(numRows):
+            # Allocating the necessary array
+            res.append([0] * (i+1))
+            res[i][0] = 1
+            res[i][-1] = 1
+
+        for i in range(numRows):
+            if i <= 1:
+                # Skip first 2 rows: 0 and 1
+                continue
+
+            for j in range(1, len(res[i]) -1):
+                print(i, j)
+                res[i][j] = res[i-1][j]+res[i-1][j-1]
+
+        return res[rowIndex]

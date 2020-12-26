98d3cc36e52e0adbf66ea6afa742ecabb620dace
hyfung <43932013+hyfung@users.noreply.github.com>
2020-12-26 18:49:39 +0800

    Create Solution.py

---
+++ 1614. Maximum Nesting Depth of the Parentheses/Solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        depth = 0
+        max_depth = 0
+        for char in s:
+            if char == '(':
+                depth += 1
+            if char == ')':
+                max_depth = max(max_depth, depth)
+                depth -= 1
+        return max_depth
+        

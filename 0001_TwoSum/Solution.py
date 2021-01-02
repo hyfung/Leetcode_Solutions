4dae16c681d1871cb3f1cd3c8dbf26347e6e11b1
hyfung <43932013+hyfung@users.noreply.github.com>
2021-01-02 22:53:49 +0800

    Update Solution.py

--- 0001_TwoSum/Solution.py
+++ 0001_TwoSum/Solution.py
@@ -1,8 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # Brute force it, simple and elegant
-        for i, i_v in enumerate(nums):
-            for j, j_v in enumerate(nums):
-                if i != j and i_v + j_v == target:
-                    return [i,j]
-        
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+                #         for i, i_v in enumerate(nums):
+#             for j, j_v in enumerate(nums):
+#                 if i > j:
+#                     continue
+#                 if i != j and i_v + j_v == target:
+#                     return [i,j]

29ea64517ac9d469a2a8fcfb612955f2daaf4df3
hyfung <43932013+hyfung@users.noreply.github.com>
2021-01-02 16:50:12 +0800

    Create Solution.py

---
+++ 1492. The kth Factor of n/Solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        count = 0
+        
+        for i in range(1, n//2+1):
+            if n % i == 0:
+                count += 1
+                
+            if count == k:
+                return i
+        
+        count += 1
+        
+        if count == k:
+            return n
+        
+        return -1
+        

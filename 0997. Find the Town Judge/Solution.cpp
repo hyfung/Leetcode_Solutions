5d157acf3c979551ea8012671f35d34926d7fc90
hyfung <43932013+hyfung@users.noreply.github.com>
2021-01-27 21:47:37 +0800

    Update Solution.cpp

--- 0997. Find the Town Judge/Solution.cpp
+++ 0997. Find the Town Judge/Solution.cpp
@@ -4,12 +4,17 @@ public:
         std::set<int> sVoted;
         std::map<int, int> mVotes;
         
+        
+        // First we build a data frame
         for (auto &t : trust)
         {
             sVoted.insert(t[0]);
             mVotes[t[1]] += 1;
         }
         
+        // Then check for two criteria
+        // 1. Judge has not voted
+        // 2. Judge has N-1 votes
         for (int i=1;i<N+1;i++)
         {
             auto search = sVoted.find(i);
@@ -19,6 +24,7 @@ public:
             }
         }
         
+        // Otherwise we return not found
         return -1;
     }
 };
